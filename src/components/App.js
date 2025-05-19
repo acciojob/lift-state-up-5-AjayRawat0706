9800efb6d15affb9fde8df453db0e11846428936
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-20 01:13:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
+
 import React, { useState } from "react";
 import LoginForm from "./LoginForm";
-import "./../styles/App.css";
+import '../styles/App.css'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    setIsLoggedIn(true);
+    setIsLoggedIn(true); // Update state when the user logs in
   };
 
   return (
     <div>
-      <h1>{isLoggedIn ? "Welcome, User!" : "Please Log In"}</h1>
-      {!isLoggedIn && <LoginForm onLogin={handleLogin} />}
+      <h1>Parent Component</h1>
+      <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
     </div>
   );
 };
