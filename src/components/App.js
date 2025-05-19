2d28589ac948db36a1d53023ae3febe47589a109
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-20 00:42:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Child from './Child'
 const App = () => {
+ const [isLoggedIn,setIsLoggedIn]=useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+     {!isLoggedIn &&
+      <Child   setIsLoggedIn={setIsLoggedIn} />
+     }
+     {isLoggedIn && <p>you are logged in</p>}
     </div>
   )
 }
